f40516dbc87033b30600ad8171ec5e32779a9a75
Wool5443 <mihsolodilov2015@gmail.com>
2024-10-03 14:55:35 +0300

    Task done

--- memory_management/reference_counting_gc/include/object_module.h
+++ memory_management/reference_counting_gc/include/object_module.h
@@ -1,65 +1,142 @@
 #ifndef MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
 #define MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
 
+#include <unistd.h>
+#include <algorithm>
 #include <cstddef>
+#include <type_traits>
+#include <utility>
+#include "Types.hpp"
 
 template <class T>
 class Object;
 
 template <class T, class... Args>
-static Object<T> MakeObject([[maybe_unused]] Args... args)
+static Object<T> MakeObject(Args... args)
 {
-    // TODO(you): Implemet this finction
-    return {};
+    return Object<T>::MakeObject(args...);
 }
 
 template <class T>
 class Object {
 public:
-    Object() = default;
-    explicit Object(std::nullptr_t) {}
-    explicit Object([[maybe_unused]] T *ptr) {}
+    Object() noexcept = default;
 
-    ~Object() = default; // this method should be changed
-
-    // copy semantic
-    Object([[maybe_unused]] const Object<T> &other) {}
-    // NOLINTNEXTLINE(bugprone-unhandled-self-assignment)
-    Object<T> &operator=([[maybe_unused]] const Object<T> &other)
+    template <class U = T, std::enable_if_t<std::is_constructible_v<T, U>>>
+    Object(U &&value)
     {
+        new (Get()) T(std::forward<U>(value));
+        getHeader_()->rc = 1;
+    }
+
+    explicit Object(std::nullptr_t) : val_(nullptr) {}
+
+    ~Object()
+    {
+        if (!val_)
+            return;
+
+        sz *rc = &getHeader_()->rc;
+
+        if (*rc == 1) {
+            getValue_()->~T();
+            delete[] (char *)val_;
+        } else {
+            --*rc;
+        }
+    }
+
+    Object(const Object &other) : val_(other.val_)
+    {
+        if (val_)
+            getHeader_()->rc++;
+    }
+
+    Object &operator=(const Object &other)
+    {
+        this->~Object();
+
+        val_ = other.val_;
+
+        if (val_)
+            getHeader_()->rc++;
+
         return *this;
     }
 
-    // move semantic
-    Object([[maybe_unused]] Object<T> &&other) {}
-    Object<T> &operator=([[maybe_unused]] Object<T> &&other)
+    Object(Object &&other) : val_(other.val_)
     {
+        other.val_ = nullptr;
+    }
+
+    Object &operator=(Object &&other)
+    {
+        std::swap(val_, other.val_);
+
         return *this;
     }
 
-    // member access operators
-    T &operator*() const noexcept {
-        return *val_;
+    T &operator*() const noexcept
+    {
+        return *getValue_();
     }
 
-    T *operator->() const noexcept {
-        return val_;
+    T *operator->() const noexcept
+    {
+        return getValue_();
     }
 
-    // internal access
-    void Reset([[maybe_unused]] T *ptr) {}
+    void Reset(T *ptr) {}
+
     T *Get() const
     {
-        return val_;
+        return getValue_();
     }
-    size_t UseCount() const
+
+    sz UseCount() const
     {
+        if (val_)
+            return getHeader_()->rc;
         return 0;
     }
 
+    template <class... Args>
+    static Object MakeObject(Args... args)
+    {
+        Object obj;
+
+        obj.val_ = allocateObj_();
+        obj.getHeader_()->rc = 1;
+
+        new (obj.getValue_()) T(args...);
+
+        return obj;
+    }
+
 private:
-    // TODO(you): Add your fields and methods here...
-    T* val_ = nullptr; // this field can be deleted
+    struct Header {
+        sz rc;
+    };
+
+    char *val_ = nullptr;
+
+    static char *allocateObj_()
+    {
+        return new char[sizeof(Header) + sizeof(T)];
+    }
+
+private:
+    Header *getHeader_() const noexcept
+    {
+        return reinterpret_cast<Header *>(val_);
+    }
+
+    T *getValue_() const noexcept
+    {
+        if (val_)
+            return reinterpret_cast<T *>(val_ + sizeof(Header));
+        return nullptr;
+    }
 };
 
 #endif  // MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
