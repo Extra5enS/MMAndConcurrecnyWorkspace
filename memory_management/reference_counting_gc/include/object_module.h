3d6e029832fbd4772486d56c82e55efc65a52fea
Wool5443 <mihsolodilov2015@gmail.com>
2024-10-04 12:50:05 +0000

    Task done

--- memory_management/reference_counting_gc/include/object_module.h
+++ memory_management/reference_counting_gc/include/object_module.h
@@ -1,65 +1,151 @@
 #ifndef MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
 #define MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
 
+#include <algorithm>
 #include <cstddef>
+#include <type_traits>
+#include <utility>
 
 template <class T>
 class Object;
 
 template <class T, class... Args>
-static Object<T> MakeObject([[maybe_unused]] Args... args)
+static Object<T> MakeObject(Args... args)
 {
-    // TODO(you): Implemet this finction
-    return {};
+    return Object<T>::MakeObject(args...);
 }
 
 template <class T>
 class Object {
 public:
-    Object() = default;
-    explicit Object(std::nullptr_t) {}
-    explicit Object([[maybe_unused]] T *ptr) {}
+    Object() noexcept = default;
 
-    ~Object() = default; // this method should be changed
-
-    // copy semantic
-    Object([[maybe_unused]] const Object<T> &other) {}
-    // NOLINTNEXTLINE(bugprone-unhandled-self-assignment)
-    Object<T> &operator=([[maybe_unused]] const Object<T> &other)
+    template <class U = T, std::enable_if_t<std::is_constructible_v<T, U>>>
+    // See
+    // https://en.cppreference.com/w/cpp/language/overload_resolution#:~:text=since%20C%2B%2B11)-,Best%20viable%20function,-For%20each%20pair
+    // NOLINTNEXTLINE(bugprone-forwarding-reference-overload)
+    explicit Object(U &&value) : val_(new T(std::forward<U>(value))), header_(new Header)
     {
+        header_->rc = 1;
+    }
+
+    explicit Object(T *ptr) : val_(ptr), header_(new Header)
+    {
+        header_->rc++;
+    }
+
+    explicit Object(std::nullptr_t) noexcept {}
+
+    ~Object()
+    {
+        if (!header_) {
+            return;
+        }
+
+        size_t &rc = header_->rc;
+
+        if (rc == 1) {
+            delete val_;
+            delete header_;
+        } else {
+            rc--;
+        }
+    }
+
+    Object(const Object &other) : val_(other.val_), header_(other.header_)
+    {
+        if (header_) {
+            header_->rc++;
+        }
+    }
+
+    Object &operator=(const Object &other)
+    {
+        if (this == &other) {
+            return *this;
+        }
+
+        this->~Object();
+
+        val_ = other.val_;
+        header_ = other.header_;
+
+        if (header_) {
+            header_->rc++;
+        }
+
         return *this;
     }
 
-    // move semantic
-    Object([[maybe_unused]] Object<T> &&other) {}
-    Object<T> &operator=([[maybe_unused]] Object<T> &&other)
+    Object(Object &&other) : val_(other.val_), header_(other.header_)
     {
+        other.val_ = nullptr;
+        other.header_ = nullptr;
+    }
+
+    Object &operator=(Object &&other)
+    {
+        if (this == &other) {
+            return *this;
+        }
+
+        std::swap(val_, other.val_);
+        std::swap(header_, other.header_);
+
         return *this;
     }
 
-    // member access operators
-    T &operator*() const noexcept {
+    T &operator*() const noexcept
+    {
         return *val_;
     }
 
-    T *operator->() const noexcept {
+    T *operator->() const noexcept
+    {
         return val_;
     }
 
-    // internal access
-    void Reset([[maybe_unused]] T *ptr) {}
+    void Reset(T *ptr)
+    {
+        this->~Object();
+
+        val_ = ptr;
+        header_ = new Header;
+        header_->rc = 1;
+    }
+
     T *Get() const
     {
         return val_;
     }
+
     size_t UseCount() const
     {
-        return 0;
+        if (header_ == nullptr) {
+            return 0;
+        }
+        return header_->rc;
+    }
+
+    template <class... Args>
+    static Object MakeObject(Args... args)
+    {
+        Object obj;
+
+        obj.val_ = new T(args...);
+        obj.header_ = new Header;
+        obj.header_->rc = 1;
+
+        return obj;
     }
 
 private:
-    // TODO(you): Add your fields and methods here...
-    T* val_ = nullptr; // this field can be deleted
+    struct Header {
+        size_t rc = 0;
+    };
+
+    T *val_ = nullptr;
+    Header *header_ = nullptr;
 };
 
 #endif  // MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
