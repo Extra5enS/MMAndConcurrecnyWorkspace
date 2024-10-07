5d3a4cda5d969897ae7a68153130c3e45be20efe
Yaroslav Kuznetsov <asklater@bk.ru>
2024-10-08 00:54:09 +0300

    implemented first version of RAII container with ref counting (ref_count_gc)

--- memory_management/reference_counting_gc/include/object_module.h
+++ memory_management/reference_counting_gc/include/object_module.h
@@ -9,57 +9,103 @@ class Object;
 template <class T, class... Args>
 static Object<T> MakeObject([[maybe_unused]] Args... args)
 {
-    // TODO(you): Implemet this finction
-    return {};
+    T *temp = new T{args...};
+    Object<T> obj{temp};
+    return obj;
 }
 
 template <class T>
 class Object {
 public:
     Object() = default;
-    explicit Object(std::nullptr_t) {}
-    explicit Object([[maybe_unused]] T *ptr) {}
+    explicit Object(std::nullptr_t) : ref_count_(new size_t) {}
+    
+    explicit Object([[maybe_unused]] T *ptr) : val_(ptr), ref_count_(new size_t)
+    {
+        (*ref_count_)++;
+    }
 
-    ~Object() = default; // this method should be changed
+    ~Object()
+    {
+        if (*ref_count_ == 1)
+        {
+            delete ref_count_;
+            delete val_;
+        }
+        else
+        {
+            (*ref_count_)--;
+        }
+    }
 
     // copy semantic
-    Object([[maybe_unused]] const Object<T> &other) {}
+    Object([[maybe_unused]] const Object<T> &other) : val_(other.val_), ref_count_(other.ref_count_) 
+    {
+        (*ref_count_)++;
+    }
+    
     // NOLINTNEXTLINE(bugprone-unhandled-self-assignment)
+
     Object<T> &operator=([[maybe_unused]] const Object<T> &other)
     {
+        (*ref_count_)--;
+        val_ = other.val_;
+        ref_count_ = other.ref_count_;
+                
         return *this;
     }
 
     // move semantic
-    Object([[maybe_unused]] Object<T> &&other) {}
-    Object<T> &operator=([[maybe_unused]] Object<T> &&other)
+    Object([[maybe_unused]] Object<T> &&other) : val_(other.val_), ref_count_(other.ref_count_) 
     {
+        other.val_ = nullptr;
+        other.ref_count_ = nullptr;
+    }
+
+    Object<T> &operator=([[maybe_unused]] Object<T> &&other) : val_(other.val_), ref_count_(other.ref_count_)
+    {
+        other.val_ = nullptr;
+        other.ref_count_ = nullptr;
+
         return *this;
     }
 
     // member access operators
-    T &operator*() const noexcept {
+    T &operator*() const noexcept 
+    {
         return *val_;
     }
 
-    T *operator->() const noexcept {
+    T *operator->() const noexcept 
+    {
         return val_;
     }
 
     // internal access
-    void Reset([[maybe_unused]] T *ptr) {}
+    void Reset([[maybe_unused]] T *ptr)
+    {
+        if (val_ == ptr)
+        {    
+            return;
+        }
+        
+        *val_ = *ptr;
+        (*ref_count_) = 1;
+    }
+
     T *Get() const
     {
         return val_;
     }
     size_t UseCount() const
     {
-        return 0;
+        return *ref_count_;
     }
 
 private:
-    // TODO(you): Add your fields and methods here...
-    T* val_ = nullptr; // this field can be deleted
+
+    size_t* ref_count_ = nullptr;
+    T* val_ = nullptr;
 };
 
 #endif  // MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
