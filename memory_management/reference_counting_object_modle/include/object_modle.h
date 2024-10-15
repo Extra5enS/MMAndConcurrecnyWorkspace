efe3b8e0d4ad9fe1050d3c05c32eb810ab76c121
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-10-15 15:48:10 +0300

    first impl

--- memory_management/reference_counting_object_modle/include/object_modle.h
+++ memory_management/reference_counting_object_modle/include/object_modle.h
@@ -2,60 +2,209 @@
 #define MEMORY_MANAGEMENT_REFERECNCE_COUNTING_OBJECT_MODLE_INCLUDE_OBJECT_MODLE_H
 
 #include <cstddef>
+#include <functional>
+#include <iostream>
 
 template <class T>
 class Object;
+class MarkWord;
+class Header;
+
+class MarkWord
+{
+public:
+    MarkWord() : count_(new size_t{1}) {}
+
+    explicit MarkWord(std::nullptr_t) {}
+
+    MarkWord([[maybe_unused]] const MarkWord &other) : count_(other.count_)
+    {
+        (*count_)++;
+    }
+
+    ~MarkWord() 
+    {
+        ProcessMemRelease();
+    }
+
+    MarkWord &operator=([[maybe_unused]] const MarkWord &other)
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+
+        ProcessMemRelease();
+
+        count_ = other.count_;
+        (*count_)++;
+
+        return *this;
+    }
+
+    MarkWord(MarkWord &&other)
+    {
+        std::swap(count_, other.count_);
+    }
+
+    MarkWord &operator=([[maybe_unused]] MarkWord &&other)
+    {
+        if (this != &other)
+        {
+            std::swap(count_, other.count_);
+        }
+
+        return *this;
+    }
+
+    size_t GetCount() const
+    {
+        if (count_ == nullptr)
+        {
+            return 0;
+        }
+
+        return *count_;
+    }
+
+private:
+    size_t *count_ = nullptr;
+
+    void ProcessMemRelease()
+    {
+        if (GetCount() <= 1)
+        {
+            delete count_;
+        }
+        else
+        {
+            (*count_)--;
+        }
+    }
+};
+
+class Header
+{
+public:
+    Header() = default;
+    ~Header() = default;
+
+    explicit Header(std::nullptr_t) : markWord_(nullptr) {}
+
+    Header([[maybe_unused]] const Header &other) : markWord_(other.markWord_) {}
+
+    Header &operator=([[maybe_unused]] const Header &other)
+    {
+        if (this != &other)
+        {
+            markWord_ = other.markWord_;
+        }
+
+        return *this;
+    }
+
+    Header(Header &&other)
+    {
+        std::swap(markWord_, other.markWord_);
+    }
+
+    Header &operator=([[maybe_unused]] Header &&other)
+    {
+        if (this != &other)
+        {
+            std::swap(markWord_, other.markWord_);
+        }
+
+        return *this;
+    }
+
+    size_t GetCount() const
+    {
+        return markWord_.GetCount();
+    }
+
+private:
+    MarkWord markWord_;
+};
 
 template <class T, class... Args>
 static Object<T> MakeObject([[maybe_unused]] Args... args)
 {
-    // TODO(you): Implement this function
-    return {};
+    T *val = new T{args...};
+    Object<T> obj{val};
+    return obj;
 }
 
 // TODO(you): Implement object base on java object representatoin
 template <class T>
 class Object {
 public:
-    Object() = default;
-    explicit Object(std::nullptr_t) {}
+    Object() : data_(nullptr), header_(nullptr) {}
 
-    ~Object() = default;  // this method should be changed
+    explicit Object(std::nullptr_t) : data_(nullptr), header_(nullptr) {}
+
+    explicit Object([[maybe_unused]] T *ptr) : data_(ptr) {}
+
+    ~Object()
+    {
+        ProcessMemRelease();
+    }
 
     // copy semantic
-    Object([[maybe_unused]] const Object<T> &other) {}
+    Object([[maybe_unused]] const Object<T> &other) : header_(other.header_), data_(other.data_) {}
+
     // NOLINTNEXTLINE(bugprone-unhandled-self-assignment)
     Object<T> &operator=([[maybe_unused]] const Object<T> &other)
     {
+        if (this == &other)
+        {
+            return *this;
+        }
+
+        ProcessMemRelease();
+
+        data_ = other.data_;
+        header_ = other.header_;
         return *this;
     }
 
     // move semantic
-    Object([[maybe_unused]] Object<T> &&other) {}
+    Object([[maybe_unused]] Object<T> &&other)
+    {
+        std::swap(header_, other.header_);
+        std::swap(data_, other.data_);
+    }
+
     Object<T> &operator=([[maybe_unused]] Object<T> &&other)
     {
+        if (this != &other)
+        {
+            std::swap(header_, other.header_);
+            std::swap(data_, other.data_);
+        }
+
         return *this;
     }
 
     // member access operators
     T &operator*() const noexcept
     {
-        return *val_;
+        return *data_;
     }
 
     T *operator->() const noexcept
     {
-        return val_;
+        return data_;
     }
 
     size_t UseCount() const
     {
-        return 0;
+        return header_.GetCount();
     }
 
     bool operator==([[maybe_unused]] const Object<T> other) const
     {
-        return false;
+        return data_ == other.data_;
     }
 
     bool operator!=(const Object<T> other) const
@@ -65,19 +214,25 @@ public:
 
     bool operator==(std::nullptr_t) const
     {
-        return false;
+        return data_ == nullptr;
     }
 
     bool operator!=(std::nullptr_t) const
     {
-        return false;
+        return data_ != nullptr;
     }
 
 private:
-    // Add your constructor
+    Header  header_; 
+    T       *data_  = nullptr;
 
-    // TODO(you): Add your fields and methods here...
-    T *val_ = nullptr;  // this field can be deleted
+    void ProcessMemRelease()
+    {
+        if (UseCount() <= 1)
+        {
+            delete data_;
+        }
+    }
 };
 
 #endif  // MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODLE_H
