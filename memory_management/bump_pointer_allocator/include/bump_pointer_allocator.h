629c660019d4a739fcfaaef2af93e6eccde78d1e
Yaroslav Kuznetsov <asklater@bk.ru>
2024-10-16 23:58:25 +0300

    implemented bump allocator: unit tests and clang tidy check passed

--- memory_management/bump_pointer_allocator/include/bump_pointer_allocator.h
+++ memory_management/bump_pointer_allocator/include/bump_pointer_allocator.h
@@ -8,35 +8,60 @@
 template <size_t MEMORY_POOL_SIZE>
 class BumpPointerAllocator {
 public:
-    BumpPointerAllocator() = default;
-    ~BumpPointerAllocator() = default;
+    BumpPointerAllocator() : startPointer_(new char[MEMORY_POOL_SIZE]) 
+    {
+        freePointer_ = startPointer_;
+    }
+
+    ~BumpPointerAllocator()
+    {
+        delete[] startPointer_;
+    }
+
     NO_COPY_SEMANTIC(BumpPointerAllocator);
     NO_MOVE_SEMANTIC(BumpPointerAllocator);
 
     template <class T = uint8_t>
-    T *Allocate([[maybe_unused]] size_t count)  // TODO(you): remove [[maybe_unused]]
+    T *Allocate(size_t count)
     {
-        // TODO(you): Add your implementation here...
-        return nullptr;
+        if (count == 0)
+        {
+            return nullptr;
+        }
+
+        size_t requiredMemSize = count * sizeof(T);
+
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        if (freePointer_ - startPointer_ + requiredMemSize > MEMORY_POOL_SIZE)
+        {
+            return nullptr;
+        }
+
+        T *allocatedMemoryAddress = reinterpret_cast<T*>(freePointer_);
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        freePointer_ += requiredMemSize;
+
+        return allocatedMemoryAddress;
     }
 
     void Free()
     {
-        // TODO(you): Add your implementation here...
+        freePointer_ = startPointer_;
     }
 
     /**
      * @brief Method should check in @param ptr is pointer to mem from this allocator
      * @returns true if ptr is from this allocator
      */
-    bool VerifyPtr([[maybe_unused]] void *ptr)  // TODO(you): remove [[maybe_unused]]
+    bool VerifyPtr(void *ptr)
     {
-        // TODO(you): Add your implementation here...
-        return false;
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        return (startPointer_ <= static_cast<char*>(ptr) && static_cast<char*>(ptr) < startPointer_ + MEMORY_POOL_SIZE);
     }
 
 private:
-    // TODO(you): Add your fields and methods here...
+    char *freePointer_ = nullptr;
+    char *startPointer_ = nullptr;
 };
 
 #endif  // MEMORY_MANAGEMENT_BUMP_POINTER_ALLOCATOR_INCLUDE_BUMP_POINTER_ALLOCATOR_H
