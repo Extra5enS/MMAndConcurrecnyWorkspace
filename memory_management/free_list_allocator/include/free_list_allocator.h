03d1dd935e9f99a45a6da1d57bfb675b55248b5d
Wool5443 <mihsolodilov2015@gmail.com>
2024-10-17 13:12:26 +0300

    Upd

--- memory_management/free_list_allocator/include/free_list_allocator.h
+++ memory_management/free_list_allocator/include/free_list_allocator.h
@@ -11,25 +11,23 @@ template <size_t ONE_MEM_POOL_SIZE>
 class FreeListAllocator {
     // here we recommend you to use class MemoryPool. Use new to allocate them from heap.
     // remember, you can not use any containers with heap allocations
-    template <size_t MEM_POOL_SIZE>
     class FreeListMemoryPool {
+        friend class FreeListAllocator;
+
         struct Block {
-            size_t size;
             Block *next;
+            size_t size;
         };
-        static constexpr Block *OCCUPIED_BLOCK = reinterpret_cast<Block *>(-1);
+        static constexpr Block *OCCUPIED_BLOCK = nullptr;
 
     public:
-        FreeListMemoryPool()
+        FreeListMemoryPool() noexcept
         {
-            if (freeListHead_ == nullptr) {
-                throw std::bad_alloc();
-            }
-            freeListHead_->size = CalculateCapacity();
+            freeListHead_->size = CalculateCapacity() - sizeof(FreeListMemoryPool);
             freeListHead_->next = freeListHead_ + 1;  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
         }
 
-        char *Allocate(size_t size)
+        char *Allocate(size_t size) noexcept
         {
             Block *prevBlock = nullptr;
             Block *block = freeListHead_;
@@ -91,46 +89,75 @@ class FreeListAllocator {
                 return false;
             }
 
-            auto block = reinterpret_cast<Block *>(ptr);
+            auto block = reinterpret_cast<const Block *>(ptr);
 
             if (block->next != OCCUPIED_BLOCK) {
                 return false;
             }
 
             // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-            return base_ <= ptr && reinterpret_cast<const char *>(ptr) + sizeof(Block) < upperBound_;
+            auto base = reinterpret_cast<const char *>(this);
+            return this <= ptr && reinterpret_cast<const char *>(ptr) + sizeof(Block) < base + CalculateCapacity();
         }
 
     private:
         // NOLINTNEXTLINE(modernize-use-nullptr)
-        void *base_ = mmap(NULL, CalculateCapacity(), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        const void *const upperBound_ = base_ + CalculateCapacity();
-        Block *freeListHead_ = reinterpret_cast<Block *>(base_);
-
-        size_t CalculateCapacity() const noexcept
-        {
-            size_t psize = getpagesize();
-            size_t cap = psize * (1 + ONE_MEM_POOL_SIZE / psize);
-            if (ONE_MEM_POOL_SIZE % psize == 0) {
-                cap -= psize;
-            }
-            return cap;
-        }
+        FreeListMemoryPool *nextPool = nullptr;
+        Block *freeListHead_ = reinterpret_cast<Block *>(this + sizeof(FreeListMemoryPool));
     };
 
 public:
-    FreeListAllocator() = default;
-    ~FreeListAllocator() = default;
+    FreeListAllocator()
+    {
+        if (firstPool_ == nullptr) {
+            throw std::bad_alloc();
+        }
+    }
+
+    ~FreeListAllocator() noexcept
+    {
+        munmap(firstPool_, CalculateCapacity());
+    }
+
     NO_MOVE_SEMANTIC(FreeListAllocator);
     NO_COPY_SEMANTIC(FreeListAllocator);
 
     template <class T = char>
-    T *Allocate(size_t count)
+    T *Allocate(size_t count) noexcept
     {
-        return nullptr;
+        MemPool *pool = firstPool_;
+
+        size_t size = count * sizeof(T);
+
+        char *allocated = pool->Allocate(size);
+
+        while (allocated == nullptr) {
+            MemPool *prevPool = pool;
+            pool = pool->nextPool;
+
+            if (pool == nullptr) {
+                pool = CreateNewPool();
+                prevPool->nextPool = pool;
+            }
+
+            allocated = pool->Allocate(size);
+        }
+
+        return reinterpret_cast<T *>(allocated);
     }
 
-    void Free(void *ptr) {}
+    void Free(void *ptr) noexcept
+    {
+        MemPool *pool = firstPool_;
+
+        while (pool) {
+            pool = pool->nextPool;
+            if (pool->VerifyPtr(ptr)) {
+                pool->Free(ptr);
+                break;
+            }
+        }
+    }
 
     /**
      * @brief Method should check in @param ptr is pointer to mem from this allocator
@@ -142,6 +169,24 @@ public:
     }
 
 private:
+    using MemPool = FreeListMemoryPool;
+    MemPool *firstPool_ = CreateNewPool();
+
+    static MemPool *CreateNewPool() noexcept
+    {
+        return reinterpret_cast<MemPool *>(
+            mmap(NULL, CalculateCapacity(), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    }
+
+    static size_t CalculateCapacity() noexcept
+    {
+        size_t psize = getpagesize();
+        size_t cap = psize * (1 + ONE_MEM_POOL_SIZE / psize);
+        if (ONE_MEM_POOL_SIZE % psize == 0) {
+            cap -= psize;
+        }
+        return cap;
+    }
 };
 
 #endif  // MEMORY_MANAGEMENT_FREE_LIST_ALLOCATOR_INCLUDE_FREE_LIST_ALLOCATOR_H
