dadfc2965cc7ed73272bcba7cda6b646c035171a
vvit19 <vitaliysavchenko555@gmail.com>
2024-10-27 19:33:42 +0300

    Implemented free list allocator

--- memory_management/free_list_allocator/tests/allocator_test.cpp
+++ memory_management/free_list_allocator/tests/allocator_test.cpp
@@ -3,7 +3,7 @@
 #include <cstddef>
 #include "memory_management/free_list_allocator/include/free_list_allocator.h"
 
-TEST(FreeListAllocatorTest, DISABLED_TemplateAllocationTest)  // remove DISABLED_ prefix to use test
+TEST(FreeListAllocatorTest, TemplateAllocationTest)  // remove DISABLED_ prefix to use test
 {
     constexpr size_t MEMORY_POOL_SIZE = 4048U;
     FreeListAllocator<MEMORY_POOL_SIZE> allocator;
@@ -29,7 +29,7 @@ TEST(FreeListAllocatorTest, DISABLED_TemplateAllocationTest)  // remove DISABLED
     allocator.Free(str2);
 }
 
-TEST(FreeListAllocatorTest, DISABLED_AllocatorMemPoolOverflowTest)  // remove DISABLED_ prefix to use test
+TEST(FreeListAllocatorTest, AllocatorMemPoolOverflowTest)  // remove DISABLED_ prefix to use test
 {
     constexpr size_t MEMORY_POOL_SIZE = 8U;
     FreeListAllocator<MEMORY_POOL_SIZE> allocator;
