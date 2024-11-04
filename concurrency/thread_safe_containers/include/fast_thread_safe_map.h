6ed67a1d570292d49cca931f11aeda03f992607e
Yaroslav Kuznetsov <asklater@bk.ru>
2024-11-04 20:45:16 +0300

    first implementation

--- concurrency/thread_safe_containers/include/fast_thread_safe_map.h
+++ concurrency/thread_safe_containers/include/fast_thread_safe_map.h
@@ -1,28 +1,42 @@
 #ifndef CONCURRENCY_THREAD_SAFE_CONTAINERS_INCLUDE_FAST_THREAD_SAFE_MAP_H
 #define CONCURRENCY_THREAD_SAFE_CONTAINERS_INCLUDE_FAST_THREAD_SAFE_MAP_H
 
-// реализуйте потоко защищенный map (unordered_map), который был бы эффективным на чтения (обычно такие структуры читают намного чаще, чем изменяют )
-
 #include <optional>
+#include <unordered_map>
 
 template<class Key, class Val>
 class ThreadSafeMap {
 public:
 
     void Insert([[maybe_unused]] Key key,[[maybe_unused]] Val val) {
-        // ... implement this
+        std::unique_lock<std::mutex> lock(mutex_);
+        map_.insert(std::make_pair(key, val));
     }
 
     // returns true if erase was completed successfully, otherwise false
     bool Erase([[maybe_unused]] Key key) {
-        // ... implement this
-        return false;
+        std::unique_lock<std::mutex> lock(mutex_);
+        
+        auto hit = map_.find(key);
+
+        if (hit == map_.end())
+            return false;
+        
+        map_.erase(hit);
+
+        return true;
     }
 
     // return Val instance if object contains it, otherwise std::nullopt
     std::optional<Val> Get([[maybe_unused]] Key key) {
-        // ... implement this
-        return std::nullopt;
+        std::unique_lock<std::mutex> lock(mutex_);
+
+        auto hit = map_.find(key);
+
+        if (hit == map_.end())
+            return std::nullopt;
+
+        return std::optional<Val>(hit->second);
     }
 
     bool Test([[maybe_unused]] Key key) {
@@ -30,6 +44,9 @@ public:
     }
 
 private:
+    std::mutex mutex_;
+    std::condition_variable condVar_;
+    std::unordered_map<Key, Val> map_;
 };
 
 #endif
--- concurrency/thread_safe_containers/include/thread_safe_queue.h
+++ concurrency/thread_safe_containers/include/thread_safe_queue.h
@@ -1,27 +1,36 @@
 #ifndef CONCURRENCY_THREAD_SAFE_CONTAINERS_INCLUDE_THREAD_SAFE_QUEUE_H
 #define CONCURRENCY_THREAD_SAFE_CONTAINERS_INCLUDE_THREAD_SAFE_QUEUE_H
 
-// реализуйте потоко защищенную очередь, которая в Pop ожидала бы появления нового элемента, если очередь пусткая
+#include <queue>
+#include <mutex>
 
 template<class T>
 class ThreadSafeQueue {
 public:
-
-    void Push([[maybe_unused]] T val) {
-        // ... implement this
+    void Push(const T& val) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        queue_.push(val);
+        condVar_.notify_one();
     }
 
     T Pop() {
-        // ... implement this
-        return T{};
+        std::unique_lock<std::mutex> lock(mutex_);
+        condVar_.wait(lock, [this] { return !queue_.empty(); });
+        
+        T value = queue_.front();
+        queue_.pop();
+        return value;
     }
 
     bool IsEmpty() {
-        // ... implement this
-        return true;
+        std::unique_lock lock(mutex_);
+        return queue_.empty();
     }
 
 private:
+    std::mutex mutex_;
+    std::condition_variable condVar_;
+    std::queue<T> queue_;
 };
 
 #endif
