833fcca2640f3bb6482d6ae7c48346bcf7edb00e
Wool5443 <mihsolodilov2015@gmail.com>
2024-11-23 22:23:10 +0300

    Fix Tody checks

--- cmake/Flags.cmake
+++ cmake/Flags.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_CXX_FLAGS_DEBUG "\
 -pthread -g -D _DEBUG -ggdb3 -std=c++17 -O0 -Wall -Wextra -Weffc++ \
--fsanitize=address,undefined,leak,shift,float-divide-by-zero,signed-integer-overflow\
+-fsanitize=thread\
 ")
 set(CMAKE_CXX_FLAGS_RELEASE "-pthread -O3 -g -DNDEBUG")
