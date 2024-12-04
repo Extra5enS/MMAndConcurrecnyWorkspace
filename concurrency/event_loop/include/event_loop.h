1a861ba260b4d68467a2ff668f32e576a1f46c96
Yaroslav Kuznetsov <asklater@bk.ru>
2024-12-04 19:42:41 +0300

    implemented even loop; tests and clang tidy passed

--- concurrency/event_loop/include/event_loop.h
+++ concurrency/event_loop/include/event_loop.h
@@ -1,44 +1,74 @@
 #ifndef CONCURRENCY_EVENT_LOOP_INCLUDE_EVENT_LOOP_H
 #define CONCURRENCY_EVENT_LOOP_INCLUDE_EVENT_LOOP_H
 
+#include <queue>
+#include <any>
+#include <utility>
+
 #include "base/macros.h"
 
-// event loop это механизм, завязанный на событиях и их асинхронной работе. Вы делаете post колбета, и он когда-нибудь исполнится
-// В нашем случае будет использоваться EventLoopScope, и все колбеки должны исполниться при разрушении EventLoopScope.
-// Для взаимодействия 
+using TaskType = std::function<void()>;
 
 class EventLoop {
 public:
     EventLoop() = default;
-    ~EventLoop() = default;
+    ~EventLoop() {
+        while (!tasks_.empty()) {
+            auto task = std::move(tasks_.front());
+            tasks_.pop();
+            task();
+        }
+    }
 
     NO_MOVE_SEMANTIC(EventLoop);
     NO_COPY_SEMANTIC(EventLoop);    
-    
+
     template<class Callback, class... Args>
     void AddCallback([[maybe_unused]] Callback callback, [[maybe_unused]] Args... args) {
-        // impl
+        auto task = [callable = std::forward<Callback>(callback)] { return callable(); };
+        tasks_.emplace(std::move(task));
     }
 private:
-    // add your fields
+    std::queue<TaskType> tasks_;
 };
 
 class EventLoopScope {
-
 public:
-    EventLoopScope() = default;
-    ~EventLoopScope() = default;
+    EventLoopScope() : oldTaskCount_(taskCount_) { taskCount_ = 0; }
+    ~EventLoopScope() {
+        size_t taskCount = taskCount_;
+        taskCount_ = oldTaskCount_;
+
+        std::vector<TaskType> temp = {};
+
+        for (size_t i = 0; i < taskCount; i++)
+        {
+            temp.push_back(std::move(tasks_.back()));
+            tasks_.pop_back();
+        }
+
+        for (size_t i = 0; i < taskCount; i++)
+        {
+            auto task = std::move(temp[taskCount - i -1]);
+            task();
+        }
+    }
 
     NO_COPY_SEMANTIC(EventLoopScope);
     NO_MOVE_SEMANTIC(EventLoopScope);
 
     template<class Callback, class... Args>
-    static void AddCallback(Callback callback, Args... args) {
-        // impl
+    static void AddCallback([[maybe_unused]] Callback callback, [[maybe_unused]] Args... args) {
+        taskCount_++;
+        auto task = [callable = std::forward<Callback>(callback)] { return callable(); };
+        tasks_.emplace_back(std::move(task));
     }
 private:
-    // add your fields
+    static std::vector<TaskType> tasks_;
+    static size_t taskCount_;
+    const size_t oldTaskCount_;
 };
 
-
+size_t EventLoopScope::taskCount_ = 0;              // NOLINT(misc-definitions-in-headers)
+std::vector<TaskType> EventLoopScope::tasks_ = {};  // NOLINT(misc-definitions-in-headers, fuchsia-statically-constructed-objects)
 #endif
