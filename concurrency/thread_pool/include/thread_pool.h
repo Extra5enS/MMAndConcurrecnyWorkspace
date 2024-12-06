ac797885986e69c97f9839c7522e3ac630ec1c43
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-12-06 17:36:31 +0300

    first impl

--- concurrency/thread_pool/include/thread_pool.h
+++ concurrency/thread_pool/include/thread_pool.h
@@ -2,27 +2,84 @@
 #define CONCURRENCY_THREAD_POOL_INCLUDE_THREAD_POOL_H
 
 #include "base/macros.h"
+#include "concurrency/thread_safe_containers/include/thread_safe_queue.h"
+
 #include <cstddef>
+#include <thread>
+#include <queue>
+#include <future>
 
 class ThreadPool {
+    using TaskType = std::function<void()>;
+
 public:
-    explicit ThreadPool([[maybe_unused]] size_t countOfTask) {
-        // impl
+    explicit ThreadPool([[maybe_unused]] size_t countOfThread) : threadCount_(countOfThread) {
+        threads_.reserve(threadCount_);
+
+        auto threadWork = [this]() -> void
+        {
+            while (true)
+            {
+                {std::unique_lock lock(mutex_);
+                cv_.wait(lock, [this]()->bool { return !tasks_.IsEmpty() || end_.load(); });}
+
+                if (!tasks_.IsEmpty())
+                {
+                    auto task = std::move(tasks_.Pop());
+                    
+                    if (task.has_value())
+                    {
+                        (*task)();
+                    }
+                }
+                else if (end_.load())
+                {
+                    tasks_.ReleaseConsumers();
+                    return;
+                }
+            }
+        };
+
+        for (size_t i = 0; i < threadCount_; i++)
+        {
+            threads_.emplace_back(threadWork);
+        }
     }
+
     ~ThreadPool() = default;
     NO_COPY_SEMANTIC(ThreadPool);
     NO_MOVE_SEMANTIC(ThreadPool);
 
     template<class Task, class... Args>
     void PostTask([[maybe_unused]] Task task, [[maybe_unused]] Args... args) {
-        // impl
-    }
+        // NOLINTNEXTLINE(modernize-avoid-bind)
+        tasks_.Push(std::bind(std::forward<Task>(task),
+                              std::forward<Args>(args)...));
 
+        {std::unique_lock lock(mutex_); cv_.notify_one();}
+    }
+    
     void WaitForAllTasks() {
-        // impl
+        end_.store(true);
+
+        {std::unique_lock lock(mutex_); cv_.notify_all();}
+
+        for (auto &thread : threads_) {
+            thread.join();
+        }
     }
 
 private:
+
+    const size_t threadCount_ = 0;
+    
+    std::vector<std::thread> threads_ = {};
+    ThreadSafeQueue<TaskType> tasks_ = {}; 
+
+    std::condition_variable cv_;
+
+    std::atomic<bool> end_{false};
+    std::mutex mutex_;
 };
 
 #endif
