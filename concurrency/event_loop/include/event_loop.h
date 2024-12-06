3717b85aebd6b30253ad75a333c1e34e542a31ed
vvit19 <vitaliysavchenko555@gmail.com>
2024-12-06 16:37:15 +0300

    Implemented event loop

--- concurrency/event_loop/include/event_loop.h
+++ concurrency/event_loop/include/event_loop.h
@@ -1,44 +1,67 @@
 #ifndef CONCURRENCY_EVENT_LOOP_INCLUDE_EVENT_LOOP_H
 #define CONCURRENCY_EVENT_LOOP_INCLUDE_EVENT_LOOP_H
 
+#include <functional>
+#include <queue>
+#include <utility>
+#include <vector>
 #include "base/macros.h"
 
 // event loop это механизм, завязанный на событиях и их асинхронной работе. Вы делаете post колбета, и он когда-нибудь исполнится
 // В нашем случае будет использоваться EventLoopScope, и все колбеки должны исполниться при разрушении EventLoopScope.
-// Для взаимодействия 
+// Для взаимодействия
 
 class EventLoop {
 public:
     EventLoop() = default;
-    ~EventLoop() = default;
-
-    NO_MOVE_SEMANTIC(EventLoop);
-    NO_COPY_SEMANTIC(EventLoop);    
-    
-    template<class Callback, class... Args>
-    void AddCallback([[maybe_unused]] Callback callback, [[maybe_unused]] Args... args) {
-        // impl
+    ~EventLoop() {
+        while (!events_.empty()) {
+            auto func = events_.front();
+            func();
+            events_.pop();
+        }
     }
+
+    NO_COPY_SEMANTIC(EventLoop);
+    NO_MOVE_SEMANTIC(EventLoop);
+
+    template<class Callback, class... Args>
+    void AddCallback(Callback callback, Args... args) {
+        auto func = (
+            [call = std::forward<Callback>(callback), &args...]
+            { return call(std::forward<Args>(args)...); } );
+
+        events_.push(std::move(func));
+    }
+
 private:
-    // add your fields
+    std::queue<std::function<void()>> events_;
 };
 
 class EventLoopScope {
-
 public:
-    EventLoopScope() = default;
-    ~EventLoopScope() = default;
+    EventLoopScope() {
+        loops_.emplace_back(new EventLoop {});
+    }
+    ~EventLoopScope() {
+        EventLoop *loop = loops_.back();
+        loops_.pop_back();
+        delete loop;
+    }
 
     NO_COPY_SEMANTIC(EventLoopScope);
     NO_MOVE_SEMANTIC(EventLoopScope);
 
     template<class Callback, class... Args>
     static void AddCallback(Callback callback, Args... args) {
-        // impl
+        loops_.back()->AddCallback(std::move(callback), std::move(args)...);
     }
+
 private:
-    // add your fields
+    static std::vector<EventLoop*> loops_;
 };
 
+// NOLINTNEXTLINE(misc-definitions-in-headers, fuchsia-statically-constructed-objects,-warnings-as-errors)
+std::vector<EventLoop*> EventLoopScope::loops_ {};
 
 #endif
