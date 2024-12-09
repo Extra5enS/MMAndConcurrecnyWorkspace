32fb7be957c1e11907c4ccff45f4571226ec1b62
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-12-09 14:04:58 +0300

    refactoring

--- concurrency/lock_free_stack/include/lock_free_stack.h
+++ concurrency/lock_free_stack/include/lock_free_stack.h
@@ -12,103 +12,32 @@
 
 const size_t MAX_THREAD_NUM = 100;
 
-template <class T>
-struct Node
+template <class PtrT>
+class HazardPtrManager
 {
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    T key;
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    Node<T> *next;
-
-    explicit Node(const T& key, Node<T> *next = nullptr) : key(key), next(next) {}
-}; // class Node
-
-template <class T>
-struct HazardPtr
-{
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    std::atomic<Node<T>*> node = nullptr;
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    std::atomic<std::thread::id> id;
-}; // class HazardPtr
-
-template <class T>
-std::array<HazardPtr<T>, MAX_THREAD_NUM> g_hazardPtrs;
-
-template <class T>
-struct DelList
-{
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    std::array<Node<T>*, 2 * MAX_THREAD_NUM + 1> delList{};
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    size_t delCount = 0;
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    std::atomic<std::thread::id> id;
-
-    void DelNode(Node<T>* node)
-    {
-        delList[delCount++] = node;
-        if (delCount >= 2 * MAX_THREAD_NUM)
-        {
-            Scan();
-        }
-    }
-
 private:
-    void Scan()
+
+    struct HazardPtr
     {
-        std::array<Node<T>*, 2 * MAX_THREAD_NUM + 1> newDelList{};
-        size_t newDelCount = 0;
-        for (size_t i = 0; i < delCount; i++)
-        {
-            if (IsHazardPtr(delList[i]))
-            {
-                newDelList[newDelCount++] = delList[i];
-            }
-            else
-            {
-                delete delList[i];
-            }
-        }
-        
-        delList = newDelList;
-        delCount = newDelCount;
-    }
-
-    bool IsHazardPtr(Node<T>* node)
-    {
-        for (size_t i = 0; i < MAX_THREAD_NUM; ++i)
-        {
-            if (g_hazardPtrs<T>[i].node.load(std::memory_order_acquire) == node)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-}; // class DelList
-
-template <class T>
-std::array<DelList<T>, MAX_THREAD_NUM> g_delLists;
-
-template <class T>
-class LockFreeStack {
+        // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
+        std::atomic<PtrT> ptr = nullptr;
+        // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
+        std::atomic<std::thread::id> id;
+    }; // class HazardPtr
 
     struct HazardPtrWrapper
     {
         // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-        HazardPtr<T>* hazardPtr = nullptr;
+        HazardPtr* hazardPtr = nullptr;
 
-        HazardPtrWrapper()
+        explicit HazardPtrWrapper(std::array<HazardPtr, MAX_THREAD_NUM>& hazardPtrs)
         {
             for (size_t i = 0; i < MAX_THREAD_NUM; ++i)
             {
                 std::thread::id id;
-                if (g_hazardPtrs<T>[i].id.compare_exchange_strong(id, std::this_thread::get_id(), std::memory_order_acquire))
+                if (hazardPtrs[i].id.compare_exchange_strong(id, std::this_thread::get_id(), std::memory_order_acquire))
                 {
-                    hazardPtr = &g_hazardPtrs<T>[i];
+                    hazardPtr = &hazardPtrs[i];
                     break;
                 }
             }
@@ -122,30 +51,78 @@ class LockFreeStack {
         ~HazardPtrWrapper()
         {
             hazardPtr->id.store(std::thread::id{}, std::memory_order_release);
-            hazardPtr->node.store(nullptr, std::memory_order_release);
+            hazardPtr->ptr.store(nullptr, std::memory_order_release);
         }
         
     }; // class HazardPtrWrapper
 
-    std::atomic<Node<T>*>& GetHazardPtr()
+    struct DelList
     {
-        thread_local static HazardPtrWrapper wrapper;
-        return wrapper.hazardPtr->node;
-    }
+        // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
+        std::array<PtrT, 2 * MAX_THREAD_NUM + 1> delList{};
+        // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
+        size_t delCount = 0;
+        // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
+        std::atomic<std::thread::id> id;
+
+        void DelNode(PtrT node, std::array<HazardPtr, MAX_THREAD_NUM>& hazardPtrs)
+        {
+            delList[delCount++] = node;
+            if (delCount >= 2 * MAX_THREAD_NUM)
+            {
+                Scan(hazardPtrs);
+            }
+        }
+
+    private:
+        void Scan(std::array<HazardPtr, MAX_THREAD_NUM>& hazardPtrs)
+        {
+            std::array<PtrT, 2 * MAX_THREAD_NUM + 1> newDelList{};
+            size_t newDelCount = 0;
+            for (size_t i = 0; i < delCount; i++)
+            {
+                if (IsHazardPtr(delList[i], hazardPtrs))
+                {
+                    newDelList[newDelCount++] = delList[i];
+                }
+                else
+                {
+                    delete delList[i];
+                }
+            }
+            
+            delList = newDelList;
+            delCount = newDelCount;
+        }
+
+        bool IsHazardPtr(PtrT ptr, std::array<HazardPtr, MAX_THREAD_NUM>& hazardPtrs)
+        {
+            for (size_t i = 0; i < MAX_THREAD_NUM; ++i)
+            {
+                if (hazardPtrs[i].ptr.load(std::memory_order_acquire) == ptr)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }; // class DelList
 
     struct DelListWrapper
     {
         // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-        DelList<T>* delList;
+        DelList* delList;
 
-        DelListWrapper()
+        explicit DelListWrapper(std::array<DelList, MAX_THREAD_NUM>& delLists)
         {
             for (size_t i = 0; i < MAX_THREAD_NUM; ++i)
             {
                 std::thread::id id;
-                if (g_delLists<T>[i].id.compare_exchange_strong(id, std::this_thread::get_id(), std::memory_order_acquire))
+                if (delLists[i].id.compare_exchange_strong(id, std::this_thread::get_id(), std::memory_order_acquire))
                 {
-                    delList = &g_delLists<T>[i];
+                    delList = &delLists[i];
                     break;
                 }
             }
@@ -162,17 +139,46 @@ class LockFreeStack {
         }
     };
 
-    DelList<T>* GetDelList()
+public:
+    std::atomic<PtrT>& GetHazardPtr()
     {
-        thread_local static DelListWrapper wrapper;
-        return wrapper.delList;
+        thread_local static HazardPtrWrapper wrapper(hazardPtrs_);
+        return wrapper.hazardPtr->ptr;
     }
 
+    void DelNode(PtrT ptr)
+    {
+        thread_local static DelListWrapper wrapper(delLists_);
+        wrapper.delList->DelNode(ptr, hazardPtrs_);
+    }
+
+private:
+
+    std::array<HazardPtr, MAX_THREAD_NUM> hazardPtrs_;
+    std::array<DelList, MAX_THREAD_NUM> delLists_;
+
+}; // class HazardPointerManager
+
+template <class T>
+class LockFreeStack {
+
+private:
+
+    struct Node
+    {
+        // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
+        T key;
+        // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
+        Node *next;
+
+        explicit Node(const T& key, Node *next = nullptr) : key(key), next(next) {}
+    }; // class Node
+    
 public:
 
     void Push(T val)
     {
-        auto *newNode = new Node<T>{val, head_.load(std::memory_order_acquire)};
+        auto *newNode = new Node{val, head_.load(std::memory_order_acquire)};
         while (!head_.compare_exchange_weak(newNode->next, newNode, std::memory_order_release))
         {
             ;
@@ -181,9 +187,8 @@ public:
 
     std::optional<T> Pop()
     {
-        Node<T> *curHead = nullptr;
-        std::atomic<Node<T>*>& hazardPtr = GetHazardPtr();
-        DelList<T>* delList = GetDelList();
+        Node *curHead = nullptr;
+        std::atomic<Node*>& hazardPtr = hpManager_.GetHazardPtr();
 
         do
         {
@@ -193,10 +198,10 @@ public:
                 return std::nullopt;
             }
 
-            Node<T> *tmp = nullptr;
+            Node *tmp = nullptr;
             do {
                 tmp = curHead;
-                hazardPtr.store(curHead, std::memory_order_release);
+                SetHazard(hazardPtr, curHead);
                 curHead = head_.load(std::memory_order_acquire);
                 if (curHead == nullptr)
                 {
@@ -207,8 +212,8 @@ public:
         } while (!head_.compare_exchange_weak(curHead, curHead->next, std::memory_order_acquire));
         
         T tmp = curHead->key;
-        hazardPtr.store(nullptr, std::memory_order_release);
-        delList->DelNode(curHead);
+        SetHazard(hazardPtr, nullptr);
+        hpManager_.DelNode(curHead);
         
         return tmp;
     }
@@ -220,7 +225,13 @@ public:
 
 private:
 
-    std::atomic<Node<T>*> head_{nullptr};
+    void SetHazard(std::atomic<Node*>& hazardPtr, Node* val)
+    {
+        hazardPtr.store(val, std::memory_order_release);
+    }
+
+    std::atomic<Node*> head_{nullptr};
+    HazardPtrManager<Node*> hpManager_;
 }; // class LockFreeStack
 
 #endif
